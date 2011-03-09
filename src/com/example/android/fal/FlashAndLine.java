dafa6da094df97ca213c63b64c5769c24392b6ed
Era3er <era3er@mail.ru>
2011-03-09 17:36:25 +0600

    Signed-off-by: Era3er <era3er@mail.ru>

---
+++ src/com/example/android/fal/FlashAndLine.java
@@ -0,0 +1,17 @@
+package com.example.android.fal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.view.ViewGroup.LayoutParams;
+
+public class FlashAndLine extends Activity 
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
